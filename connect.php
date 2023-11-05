b35cc0682ad21056cc1e90f7b70f0f5ffa140723
nazim <nazim.saifi1908@gmail.com>
2023-11-05 15:28:45 +0530

    sdgfcj

--- connect.php
+++ connect.php
@@ -11,7 +11,7 @@
 		echo "$conn->connect_error";
 		die("Connection Failed : ". $conn->connect_error);
 	} else {
-		$stmt = $conn->prepare("insert into registration(fullname,  email, phone, subject, message ) values(?, ?, ?, ?, ?)");
+		$stmt = $conn->prepare("insert into contact(fullname,  email, phone, subject, message ) values(?, ?, ?, ?, ?)");
 		$stmt->bind_param("ssiss", $fullname,  $email,  $phone,  $subject,  $message);
 		$execval = $stmt->execute();
 		echo $execval;
